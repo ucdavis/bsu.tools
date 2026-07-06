eaf98564c6632386eb3e31a1a15187a60c299f4b
Matt <mdponzini@ucdavis.edu>
2026-07-06 16:21:24 -0700

    start work on function to construct basic summaries (table and figures)

---
+++ R/basic_summaries.R
@@ -0,0 +1,28 @@
+basic_summaries <- function(data, vars = colnames(data), by_var = NULL) {
+  # create summary table using gtsummary::tbl_summary()
+  tbl <- basic_summary_table(
+    data = data,
+    vars = vars,
+    by_var = by_var
+  )
+  # create summary figures (histogram for numeric, bar plot for categorical)
+  figs <- basic_summary_figures(
+    data = data,
+    vars = vars,
+    by_var = by_var
+  )
+  # convert tbl to gt object and add figures
+  tbl_gt <- tbl |>
+    gtsummary::as_gt() |>
+    gt::cols_add(
+      Figures = NA
+    ) |>
+    gt::text_transform(
+      locations = gt::cells_body(columns = "Figures"),
+      fn = function(x) {
+        figs |> gt::ggplot_image()
+      }
+    )
+
+  return(tbl_gt)
+}
---
+++ R/basic_summaries_utils.R
@@ -0,0 +1,65 @@
+#### basic summaries utils ####
+basic_summary_table <- function(data, vars = colnames(data), by_var = NULL) {
+  # remove by_var from vars if it exists
+  if (!is.null(by_var)) {
+    vars <- base::setdiff(vars, by_var)
+  }
+  # create initial summary table
+  tbl_summary <- data |>
+    gtsummary::tbl_summary(
+      by = dplyr::all_of(by_var),
+      include = dplyr::all_of(vars)
+    )
+
+  # add overall if by_var is not NULL
+  if (!is.null(by_var)) {
+    tbl_summary <- tbl_summary |>
+      gtsummary::add_overall()
+  }
+
+  return(tbl_summary)
+}
+
+basic_summary_figures <- function(data, vars = colnames(data), by_var = NULL) {
+  # remove by_var from vars if it exists
+  if (!is.null(by_var)) {
+    vars <- base::setdiff(vars, by_var)
+  }
+  # store class to plot appropriate figure type
+  var_class <- sapply(data[, vars], class)
+  # if by_var is not NULL and is continuous, convert to factor
+  if (
+    !is.null(by_var) &&
+    class(data[[by_var]]) %in% c("numeric", "integer", "double")
+  ) {
+    data <- data |>
+      dplyr::mutate(
+        !!{{ by_var }} := .data[[by_var]] |> as.factor()
+      )
+  }
+
+  figs <- lapply(
+    vars,
+    function(x) {
+      # if the variable is numeric, create a boxplot
+      if (var_class[x] %in% c("numeric", "integer")) {
+        data |>
+          ggplot2::ggplot() +
+          ggplot2::aes(x = .data[[by_var]], y = .data[[x]]) +
+          ggplot2::geom_boxplot() +
+          ggplot2::theme_classic()
+      } else {
+        # if the variable is categorical, create a bar plot
+        data |>
+          ggplot2::ggplot() +
+          ggplot2::aes(x = .data[[x]], fill = .data[[by_var]]) +
+          ggplot2::geom_bar(position = "dodge") +
+          ggplot2::theme_classic()
+      }
+    }
+  )
+  # add names to list
+  names(figs) <- vars
+
+  return(figs)
+}
